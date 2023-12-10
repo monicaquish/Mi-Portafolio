5e5378da310509df640d0241d3e609449ea886ab
Mónica Quiroz Santander <151891471+monicaquish@users.noreply.github.com>
2023-12-09 22:07:25 -0600

    Mi portafolio

---
+++ main.js
@@ -0,0 +1,14 @@
+let app = document.getElementById('typewriter');
+ 
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+ cursor: "<span style='color: #fb4993;'>|</span>",
+});
+ 
+typewriter
+  .pauseFor(2500)
+  .typeString('<span style="color: #fb4993;">Soy estudiante de Ingeniería de Software y Redes.</span>')
+  .pauseFor(200)
+  .deleteChars(10)
+  .start();
